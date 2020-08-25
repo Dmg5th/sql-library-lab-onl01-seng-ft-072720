a7d898e9bfbeeaeac952764fde9a4817385b352c
Daniel Gould <dmgould310@gmail.com>
2020-08-25 20:02:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,22 +35,22 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title
-   FROM series  
-   INNER JOIN books 
-   ON books.series_id = series.id
-   INNER JOIN character_books
-   ON character_books.book_id = books.id 
-   INNER JOIN characters 
-   ON character_books.character_id = characters.id 
-   WHERE character.species = 'human'
-   GROUP BY series title 
-   ORDER BY COUNT(characters.species) DESC 
-   LIMIT 1;"
+
+   "SELECT series.title 
+    FROM series 
+    JOIN books 
+    ON books.series_id = series.id 
+    JOIN character_books 
+    ON character_books.book_id = books.id 
+    JOIN characters 
+    ON character_books.character_id = characters.id 
+    WHERE characters.species = 'human' 
+    GROUP BY series.title 
+    ORDER BY COUNT(*) DESC LIMIT 1"
 end
 
 
-# SELECT series.title FROM series JOIN books ON books.series_id = series.id JOIN character_books ON character_books.book_id = books.id JOIN characters ON character_books.character_id = characters.id WHERE characters.species = 'human' GROUP BY series.title ORDER BY COUNT(*) DESC LIMIT 1""
+# 
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, "
