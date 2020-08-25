305bfcf37bbb22c6df41efc5906689997046cddc
Daniel Gould <dmgould310@gmail.com>
2020-08-25 20:08:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -52,5 +52,8 @@ end
 # 
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(character_books. "
+  "SELECT characters.name, COUNT(character_books.book_id)
+   FROM characters 
+   INNER JOIN character_books
+   ON "
 end
